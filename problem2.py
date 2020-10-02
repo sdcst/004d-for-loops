3b6d3fc372a4fea1108392fca41543c106893e51
Chris Yang <cyang@deltalearns.ca>
2020-10-02 09:56:17 -0700

    assignment update

---
+++ problem2.py
@@ -0,0 +1,28 @@
+#!python3
+
+"""
+##### Problem 2
+Calculate the factorial of a number. 
+
+ 4! does not mean 4 is excited, it's the notation for a factorial.
+A factorial is a special operation in math that is used in probability 
+and combination problems.  For example, if you have 4 people, the number
+of ways that they can be lined up against the wall is 4!
+
+A factorial is defined as:
+5! = 1 * 2 * 3 * 4 * 5
+5! = 120
+
+3! = 1 * 2 * 3
+3! = 6
+
+inputs:
+int number
+
+outputs:
+"xx! is yy" where xx is the integer entered and yy is the calculated answer
+
+example:
+Enter a number: 4
+4! is 24
+"""
---
+++ task1.py
@@ -0,0 +1,19 @@
+#!python3 
+
+"""
+##### Task 1
+Ask the user to enter an integer.
+Print the multiplication tables up to 12 for that number
+using a for loop instead of a while loop.
+(2 points)
+
+inputs:
+int number
+
+outputs:
+multiples of that number
+
+example:
+Enter number:4
+4 8 12 16 20 24 28 32 36 40 44 48
+"""
